e0cc1a27660900f5fb96cc3225ffd8eac672b183
rajat6235 <guptarajat234@gmail.com>
2022-04-21 21:36:15 +0530

    changes

--- myportfolio/src/Container/Home/Footer/Footer.js
+++ myportfolio/src/Container/Home/Footer/Footer.js
@@ -3,8 +3,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footer-container'>
-        
-
+<img src ={require ('../../../assets/Home/shape-bg.png')} alt ='no internet connection'/>
     </div>
   )
 }

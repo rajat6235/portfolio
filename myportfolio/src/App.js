5e24638b7796895a80e85e67a943f940b5da3f14
rajat6235 <guptarajat234@gmail.com>
2022-04-22 20:10:16 +0530

    footer added

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import Profile from './Container/Home/Profile';
-
+import Footer from './Container/Home/Footer/Footer';
 function App() {
   return (
     <div className="App">
      <Profile/>
-
+<Footer/>
     </div>
   );
 }
--- myportfolio/src/Container/Home/Footer/Footer.js
+++ myportfolio/src/Container/Home/Footer/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 export default function Footer() {
   return (
     <div className='footer-container'>
-<img src ={require ('../../../assets/Home/shape-bg.png')} alt ='no internet connection'/>
+<img src ={require ('../../../assets/Home/shape-bg.png').default } alt ='no internet connection'/>
     </div>
   )
 }

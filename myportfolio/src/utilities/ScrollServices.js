2bf83f32eb2d65f56296adb254f014851f755021
rajat6235 <guptarajat234@gmail.com>
2022-04-27 12:49:50 +0530

    observables added

--- myportfolio/src/utilities/ScrollServices.js
+++ myportfolio/src/utilities/ScrollServices.js
@@ -16,4 +16,56 @@ scrollToHireMe= () =>{
     if (!contactMeScreen) return ;
     contactMeScreen.scrollIntoView({behavior:"smooth"})
 }
+scrollToHome= () =>{
+    let  homeScreen =  document.getElementById("Home")
+    if (!homeScreen) return ;
+    homeScreen.scrollIntoView({behavior:"smooth"})
+}
+isElementInView = (elem , type ) =>{
+    let rec = elem.getBoundingClientRect();
+    let elementTop = rec.top;
+    let elementBottom = rec.Bottom;
+    
+    let partiallyVisible = elementTop< window.innerHeight && elementBottom>=0;
+    let completelyVisible = elementTop >= 0 && elementBottom <= window.innerHeight;
+
+    switch(type){
+        case "partial":
+            return partiallyVisible;
+            case "complete":
+            return completelyVisible;
+            default : 
+            return false
+    }
+
+}
+
+checkCurrentScreenUnderViewPort = (event) => {
+    if (!event || Object.keys(event).length<1)
+    return;
+    for (let screen of TOTAL_SCREENS){
+        let screenFromDom = document.getElementById(screen.screen_name);
+        if (!screenFromDom)
+        continue;
+
+        let fullyvisible = this.isElementInView(screenFromDom, "complete");
+        let partiallyVisible = this.isElementInView(screenFromDom, "partial");
+        if (fullyvisible || partiallyVisible){
+            if (partiallyVisible&& !screen.alreadyRendered){
+                ScrollService.currentScreenFadeIn.next({
+                    fadeInScreen : screen.screen_name
+                });
+                screen['alreadyRendere']=true;
+                break;
+            }
+            if (fullyvisible){
+                ScrollService.currentScreenBroadCaster.next({
+                    screenInView : screen.screen_name
+                });
+                break;
+            }
+        }
+
+    }
+}
 }

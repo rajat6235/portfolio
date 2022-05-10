b75665a41cf932a6d3d3e625a6daac5b9b16cbca
rajat6235 <guptarajat234@gmail.com>
2022-05-10 19:39:19 +0530

    headings

---
+++ myportfolio/src/utilities/ScreenHeadings/ScreenHeading.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ScreenHeading(props) {
+  return (
+    <div className='heading-container'>
+        <div className='screen-heading'>
+            <span> {props.title}</span>
+        </div>
+        {
+            (props.subHeading)?(
+                <div className='screen-sub-heading'>
+                    <span>{props.subHeading}</span>
+                    </div>):
+
+            <div></div>
+        }
+    </div>
+  )
+}

a7758a45d0c10acacddad6ee7ba896b00af52c40
rajat6235 <guptarajat234@gmail.com>
2022-06-05 06:59:48 +0530

    added state variables in resume component

--- myportfolio/src/Container/Resume/Resume.js
+++ myportfolio/src/Container/Resume/Resume.js
@@ -7,6 +7,33 @@ export default function Resume(props) {
   const [selectedBulletIndex,setSelectedBulletIndex]= useState(0)
   const [carousalOffSetStyle, setCarausalOffSetStyle]= useState({})
   
+  const ResumeHeading = (props) => {
+      <div className='resume-heading'>
+          <div className='resume-main-heading'>
+              <div className='heading-bullet'>
+                   <span> {props.heading ? props.heading : ''}</span>
+                   {props.fromDate && props.toDate ? (
+                       <div className='heading-date'>
+                           {props.fromDate + "_" + props.toDate}
+                           </div>
+                   ): (
+                       <div>
+                           </div>
+                   )}
+                   </div>
+                   <div className='resume-sub-heading'>
+                       <span>
+                           {props.subHeading ? props.subHeading : ''}
+                       </span>
+                   </div>
+                   <div className='resume-heading-description'>
+                       <span>
+                           {props.description ? props.description :''}
+                       </span>
+                   </div>
+          </div>
+      </div>
+  }
   let fadeInScreenHandler =(screen)=>{
     if (screen.fadeScreen !== props.id)
     return

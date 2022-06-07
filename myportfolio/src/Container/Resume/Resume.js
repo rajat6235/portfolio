b61e7771c242e70061e46c5644dee13cc0d6fb56
rajat6235 <guptarajat234@gmail.com>
2022-06-07 12:55:21 +0530

    resume content added

--- myportfolio/src/Container/Resume/Resume.js
+++ myportfolio/src/Container/Resume/Resume.js
@@ -7,7 +7,15 @@ export default function Resume(props) {
   const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
   const [carousalOffSetStyle, setCarausalOffSetStyle] = useState({});
 
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
   const ResumeHeading = (props) => {
+      return(
     <div className="resume-heading">
       <div className="resume-main-heading">
         <div className="heading-bullet">
@@ -27,7 +35,8 @@ export default function Resume(props) {
           <span>{props.description ? props.description : ""}</span>
         </div>
       </div>
-    </div>;
+    </div>
+    )
   };
 
   const resumeBullets = [
@@ -104,91 +113,125 @@ export default function Resume(props) {
         <span className="resume-description-text">
           Worked as Front End Developer
         </span>
-        <div className="experience-description">
-          <spam className="resume-decription-text">
-            - Worked on company's multiple products like Vectio Home- Health and
-            Vectio Client Portal.
-          </spam>
-          <br/>
-          <spam className="resume-decription-text">
-          Built reusable components using javaScript libraries like react
- big-calendar, react ag-grid, redux, graphql, redux-saga etc.
-          </spam>
-          <br/>
-          <spam className="resume-decription-text">
-          - Implemented new features on existing product which are
- now in production and being used by multiple clients.
-          </spam>
-          <br/>
-          <spam className="resume-decription-text">
-        - Created products with mobile-responsive and mobile-
- adaptive Ul.
-          </spam>
-          <br/>
-          <spam className="resume-decription-text">
-         - Improved the performance of the existing products by
- refactoring legacy code
-          </spam>
-          <br/>
-          </div>
-        </div>,
-        <div className="resume-screen-container programming-skills-container" 
-        key ="programming-skills">
-            {programmingSkillDetails.map((skill,index)=>(
-                <div className="skill-parent" key={index}>
-                    <div className="heading-bullet">
-                        <span>
-                            {skill.skill}
-                        </span>
-                        <div className="skill-percentage">
-                            <div style={{width: skill.ratingPercentage + "%"}}
-                            className='active-percentage'>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            ))}
-        </div>,
-        <div className="resume-screen-container" key ="projects">
-            {projectDetails.map((projectDetails,index)=>(
-                <ResumeHeading
-                key={index}
-                heading={projectDetails.title}
-                subHeading={projectDetails.subHeading}
-                description={projectDetails.description}
-                fromDate={projectDetails.duration.fromDate}
-                toDate={projectDetails.duration.toDate}
-                />
-            ))}
-        </div>,
-        <div className="resume-screen-container" key ="interests">
-<ResumeHeading
-heading='Gyming'
-description= 'Apart from being tech ethusiast and coder, I also love to work-out in gym and stay fit.'
-/>
-<ResumeHeading
-heading='Cooking'
-description= 'I like to cook healthy food.'
-/>
-<ResumeHeading
-heading='Video Games'
-description= 'I like to play video-games in my free time.'
-/>
-</div>
       </div>
-    
+      <div className="experience-description">
+        <span className="resume-decription-text">
+          - Worked on company's multiple products like Vectio Home- Health and
+          Vectio Client Portal.
+        </span>
+        <br />
+        <span className="resume-decription-text">
+          Built reusable components using javaScript libraries like react
+          big-calendar, react ag-grid, redux, graphql, redux-saga etc.
+        </span>
+        <br />
+        <span className="resume-decription-text">
+          - Implemented new features on existing product which are now in
+          production and being used by multiple clients.
+        </span>
+        <br />
+        <span className="resume-decription-text">
+          - Created products with mobile-responsive and mobile- adaptive Ul.
+        </span>
+        <br />
+        <span className="resume-decription-text">
+          - Improved the performance of the existing products by refactoring
+          legacy code
+        </span>
+      </div>
+      ,
+      <div
+        className="resume-screen-container programming-skills-container"
+        key="programming-skills"
+      >
+        {programmingSkillDetails.map((skill, index) => (
+          <div className="skill-parent" key={index}>
+            <div className="heading-bullet">
+              <span>{skill.skill}</span>
+              <div className="skill-percentage">
+                <div
+                  style={{ width: skill.ratingPercentage + "%" }}
+                  className="active-percentage"
+                ></div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      ,
+      <div className="resume-screen-container" key="projects">
+        {projectDetails.map((projectDetails, index) => (
+          <ResumeHeading
+            key={index}
+            heading={projectDetails.title}
+            subHeading={projectDetails.subHeading}
+            description={projectDetails.description}
+            fromDate={projectDetails.duration.fromDate}
+            toDate={projectDetails.duration.toDate}
+          />
+        ))}
+      </div>
+      ,
+      <div className="resume-screen-container" key="interests">
+        <ResumeHeading
+          heading="Gyming"
+          description="Apart from being tech ethusiast and coder, I also love to work-out in gym and stay fit."
+        />
+        <ResumeHeading
+          heading="Cooking"
+          description="I like to cook healthy food."
+        />
+        <ResumeHeading
+          heading="Competitive Gaming"
+          description="I like to challenge my reflexes a lot while playing competitive games."
+        />
+      </div>
+    </div>,
   ];
-  let fadeInScreenHandler = (screen) => {
-    if (screen.fadeScreen !== props.id) return;
-    Animations.animations.fadeInScreen(props.id);
+  const handleCrousal = (index) => {
+    let offSetHeight = 360;
+    let newCrousalOffSet = {
+      style: { transform: "translateY(" + index * offSetHeight * -1 + "px)" },
+    };
+    setCarausalOffSetStyle(newCrousalOffSet);
+    setSelectedBulletIndex(index);
   };
-  const fadeInSubscription =
-    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const getBullets = () => {
+    return resumeBullets.map((bullet, index) => (
+      <div onClick={() => handleCrousal(index)}
+      className={index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"}
+     key={index}>
+         <img className="bullet-logo"
+        //  src ={require(`../../assets/Resume/${bullet.logoSrc}`)}
+         alt='no internet connection...'/>
+     </div>
+    ));
+  }
+  const getResumeScreen = () =>{
+      return(
+          <div
+          style={carousalOffSetStyle.style}
+          className='resume-details-carousal'>
+              {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+          </div>
+      )
+  }
+
 
   return (
     <div className="resume-container screen-container" id={props.id || ""}>
       <div className="resume-content">
         <ScreenHeading title={"Resume"} subHeading={"My Formal Bio Details"} />
+        <div className="resume-card">
+            <div className="resume-bullets">
+                <div className="bullet-container">
+                    <div className='bullet-icons'></div>
+                    <div className='bullets'>{getBullets()}</div>
+                </div>
+            </div>
+            <div className="resume-bullet-details">{getResumeScreen()}</div>
+        </div>
       </div>
     </div>
   );

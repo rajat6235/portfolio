5bf559f839e92338017bab3b7457982e131f44fe
rajat6235 <guptarajat234@gmail.com>
2022-06-15 10:55:43 +0530

    client carousal added

--- myportfolio/src/Container/Testimonial/Testimonial.js
+++ myportfolio/src/Container/Testimonial/Testimonial.js
@@ -2,7 +2,6 @@ import React from "react";
 import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-
 import ScreenHeading from "../../utilities/ScreenHeadings/ScreenHeading";
 import ScrollService from "../../utilities/ScrollServices";
 import Animations from "../../utilities/Animations";
@@ -14,6 +13,28 @@ function Testimonial(props) {
   };
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+    const options = {
+        loop: true,
+        margin:0,
+        nav:true,
+        animateIn: "bounceInRight",
+        animateOut: "bounceOutRight",
+        dots: true,
+        autoplay:true,
+        smartSpeed:"1000",
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:1,
+            },
+            1000:{
+                items:3,
+            },
+        }
+    }
   return (
     <div>
       <ScreenHeading
@@ -23,37 +44,119 @@ function Testimonial(props) {
       <section className="testimonial-section" id={props.id || ""}>
         <div className="container">
           <div className="row">
-            <OwlCarousel className="owl-carousel" id="testimonial-carousel">
+            <OwlCarousel className="owl-carousel" id="testimonial-carousel" 
+            {...options}>
               <div className="col-lg-12">
                 <div className="testi-item">
                   <div className="testi-comment">
                     <p>
-                    <i className="fa fa-quote-left" />
+                      <i className="fa fa-quote-left" />
                       I patronized Rajat and when He delivered, I honestly fell
                       in love with the project He is a very honest guy and he
                       delivers ontime.
                       <i className="fa fa-quote-right" />
                     </p>
                     <ul className="stars list-unstyled">
-                        <li>
-                            <i className="fa fa-star"/>
-                        </li>
-                        <li>
-                            <i className="fa fa-star"/>
-                        </li>
-                        <li>
-                            <i className="fa fa-star"/>
-                        </li>
-                        <li>
-                            <i className="fa fa-star-half-alt"/>
-                        </li>
-                        <li>
-                            <i className="fa fa-star"/>
-                        </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
                     </ul>
                   </div>
                   <div className="client-info">
-                      
+                    <img
+                      src="img/Testimonial/lady.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Daisy Dominic</h5>
+                    <p> CEO Inaninglobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Rajat and when He delivered, I honestly fell
+                      in love with the project He is a very honest guy and he
+                      delivers ontime.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/Testimonial/mike.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Daisy Dominic</h5>
+                    <p> CEO Inaninglobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      I patronized Rajat and when He delivered, I honestly fell
+                      in love with the project He is a very honest guy and he
+                      delivers ontime.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img
+                      src="img/Testimonial/man.png"
+                      alt="no internet connection"
+                    ></img>
+                    <h5>Daisy Dominic</h5>
+                    <p> CEO Inaninglobal</p>
                   </div>
                 </div>
               </div>

1e90104f4b1b6d383d33a65c2858b48933ff9098
rajat6235 <guptarajat234@gmail.com>
2022-06-29 20:53:43 +0530

    server initialized

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config
+const express = require("express")
+const cors = require("cors")
+const path = require("path")
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`listening to port 5000`));

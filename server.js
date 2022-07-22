232aca89d53a11b396efea71a3c1c00713ea31c7
rajat6235 <guptarajat234@gmail.com>
2022-07-22 18:35:56 +0530

    production fixes

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-require("dotenv").config
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const contactRoute =require('./route/contactRoute')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const contactRoute =require('./route/contactRoute');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/",contactRoute)
+app.use("/",contactRoute);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("myportfolio/build"));
+    app.get("*", (req, res) =>
+      res.sendFile(path.resolve(__dirname, "myportfolio", "build", "index.html"))
+    );
+  }
 
 const port = process.env.PORT || 6000;
 app.listen(port, console.log(`listening to port 6000`));

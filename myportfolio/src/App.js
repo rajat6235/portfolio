d1de6c7262bb791c8bd4a77cacce0163c7ebbec1
rajat6235 <guptarajat234@gmail.com>
2022-09-06 12:19:26 +0530

    scroll to top added

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PortfolioContainer from './Container/PortfolioContainer';
+import ScrollToTop from './Container/ScrollToTop/ScrollToTop'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <ToastContainer/>
 <PortfolioContainer/>
+<ScrollToTop/>
     </div>
   );
 }

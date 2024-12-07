f3f3682f05d3c2291c0593f96446ffcdd5052bc8
Rajat Gupta <guptarajat234@gmail.com>
2024-12-07 12:21:30 +0530

    carousel loading fix

--- myportfolio/src/Container/Carousel/Carousel.js
+++ myportfolio/src/Container/Carousel/Carousel.js
@@ -1,44 +1,80 @@
-import React from "react";
-import Carousel from 'react-bootstrap/Carousel'
-import ScrollDown  from "../../utilities/ScrollDown/ScrollDown";
-
-
+import React, { useState, useEffect } from "react";
+import Carousel from 'react-bootstrap/Carousel';
+import ScrollDown from "../../utilities/ScrollDown/ScrollDown";
 import "./carousal.css";
-
-import  Slide1  from "../../assets/carousal/slide1.webp";
-import  Slide2  from "../../assets/carousal/slide2.webp";
-import  Slide3  from "../../assets/carousal/slide3.webp";
-import Footer from "../Home/Footer/Footer";
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Import images
+const slides = [
+  {
+    src: require("../../assets/carousal/slide1.webp"),
+    alt: "First slide"
+  },
+  {
+    src: require("../../assets/carousal/slide2.webp"),
+    alt: "Second slide"
+  },
+  {
+    src: require("../../assets/carousal/slide3.webp"),
+    alt: "Third slide"
+  }
+];
 
 const CustomCarousel = () => {
-    return (
-        <div id="home">
-            <Carousel controls={false} indicators  interval={2500} pause={false} fade>
-                <Carousel.Item>
-                    <img className="d-block w-100 custom-img " src={Slide1} alt="First slide"/>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img "
-                        src={Slide2}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100 custom-img "
-                        src={Slide3}
-                        alt="Third slide"
-                    />
-                </Carousel.Item>
-            </Carousel>
-            <ScrollDown/>
-            {/* <Footer/> */}
-        </div>
-    )
-}
+  const [loading, setLoading] = useState(true);
+  const [loadedImages, setLoadedImages] = useState(0);
 
-export default CustomCarousel
+  useEffect(() => {
+
+    slides.forEach(slide => {
+      const img = new Image();
+      img.src = slide.src;
+      img.onload = () => {
+        setLoadedImages(prev => {
+          const newCount = prev + 1;
+          if (newCount === slides.length) {
+            setLoading(false);
+          }
+          return newCount;
+        });
+      };
+    });
+  }, []);
+
+  return (
+    <div id="home" className="carousel-container">
+      {loading && (
+        <div className="loader-container">
+          <div className="loader"></div>
+          <div style={{ color: 'white', marginLeft: '10px' }}>
+            Loading... {Math.round((loadedImages / slides.length) * 100)}%
+          </div>
+        </div>
+      )}
+      
+      <Carousel 
+        controls={false} 
+        indicators 
+        interval={2500} 
+        pause={false} 
+        fade
+        style={{ visibility: loading ? 'hidden' : 'visible' }}
+      >
+        {slides.map((slide, index) => (
+          <Carousel.Item key={index}>
+            <img
+              className="d-block custom-img"
+              src={slide.src}
+              alt={slide.alt}
+              loading="lazy"
+            />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      
+      <ScrollDown />
+    </div>
+  );
+};
+
+export default CustomCarousel;

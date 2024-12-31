78bf2661d7475bedaca4c5fb794d370d486dd4a5
rajat6235 <guptarajat234@gmail.com>
2024-12-31 20:09:19 +0530

    button animation added

--- myportfolio/src/Container/AboutMe/AboutMe.tsx
+++ myportfolio/src/Container/AboutMe/AboutMe.tsx
@@ -1,87 +1,81 @@
-import React from "react";
-import ScreenHeading from "../../utilities/ScreenHeadings/ScreenHeading.tsx";
-import ScrollService from "../../utilities/ScrollService.ts";
-import Animations from "../../utilities/Animations.ts";
-import "./AboutMe.css";
-import Tech from "../TechIcons/Tech.tsx";
+import React from 'react';
+import ScreenHeading from '../../utilities/ScreenHeadings/ScreenHeading.tsx';
+import ScrollService from '../../utilities/ScrollService.ts';
+import Animations from '../../utilities/Animations.ts';
+import './AboutMe.css';
+import Tech from '../TechIcons/Tech.tsx';
 
 interface AboutMeProps {
-  id: string;
+	id: string;
 }
 
 const AboutMe: React.FC<AboutMeProps> = (props) => {
-  const fadeInScreenHandler = (screen: { fadeInScreen: string }) => {
-    if (screen.fadeInScreen !== props.id) return;
-    Animations.animations.fadeInScreen(props.id);
-  };
+	const fadeInScreenHandler = (screen: { fadeInScreen: string }) => {
+		if (screen.fadeInScreen !== props.id) return;
+		Animations.animations.fadeInScreen(props.id);
+	};
 
-  const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+	const fadeInSubscription = ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-  React.useEffect(() => {
-    return () => {
-      fadeInSubscription.unsubscribe();
-    };
-  }, [fadeInSubscription]);
+	React.useEffect(() => {
+		return () => {
+			fadeInSubscription.unsubscribe();
+		};
+	}, [fadeInSubscription]);
 
-  const SCREEN_CONSTANTS = {
-    description:
-      "I am a highly skilled Full Stack Web Developer, currently based in Mohali, Punjab, India. My primary focus is on Front-End Development, and I bring a comprehensive understanding of various front-end and back-end languages, responsive frameworks, and database management systems. I aspire to excel in the field of web development and make impactful contributions to the ever-evolving technology industry.",
-    highlights: {
-      bullets: [
-        "Full stack Web and mobile development",
-        "Interactive Front End as per design",
-        "Redux for state management",
-        "Building Rest API",
-        "Managing database",
-      ],
-      heading: "Here are a Few Highlights:",
-    },
-  };
+	const SCREEN_CONSTANTS = {
+		description:
+			'With a track record of successful projects, I bring a wealth of experience and reliability to the table. My commitment to excellence and attention to detail ensure that every application is built to the highest standards. I am dedicated to delivering solutions that are robust, scalable, and tailored to meet the unique needs of each client',
+		highlights: {
+			bullets: [
+				'User Experience Focus',
+				'Quality Assurance',
+				'Continuous Learning',
+				'Effective Client Communication',
+				'Collaborative Spirit',
+			],
+			heading: 'Here are a Few Highlights:',
+		},
+	};
 
-  const renderHighlight = () => {
-    return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
-      <div className="highlight" key={i}>
-        <div className="highlight-blob"></div>
-        <span>{value}</span>
-      </div>
-    ));
-  };
+	const renderHighlight = () => {
+		return SCREEN_CONSTANTS.highlights.bullets.map((value, i) => (
+			<div className='highlight' key={i}>
+				<div className='highlight-blob'></div>
+				<span>{value}</span>
+			</div>
+		));
+	};
 
-  return (
-    <div className="about-me-container screen-container fade-in" id={props.id || ""}>
-      <div className="about-me-parent">
-        <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
-        <div className="about-me-card">
-          <div className="about-me-details">
-            <span className="about-me-description">
-              {SCREEN_CONSTANTS.description}
-            </span>
-            <div className="about-me-highlights">
-              <div className="highlight-heading">
-                <span>{SCREEN_CONSTANTS.highlights.heading}</span>
-              </div>
-              {renderHighlight()}
-            </div>
-            <div className="about-me-options">
-              <button
-                className="btn primary-btn"
-                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
-              >
-                Hire Me
-              </button>
-              <a
-                href="RajatGuptaResume2022.pdf"
-                download="RajatGuptaResume2022.pdf"
-              >
-                <button className="btn highlighted-btn">Download CV</button>
-              </a>
-            </div>
-          </div>
-          <Tech />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='about-me-container screen-container fade-in' id={props.id || ''}>
+			<div className='about-me-parent'>
+				<ScreenHeading title={'About Me'} subHeading={'Why Choose Me?'} />
+				<div className='about-me-card'>
+					<div className='about-me-details'>
+						<span className='about-me-description'>{SCREEN_CONSTANTS.description}</span>
+						<div className='about-me-highlights'>
+							<div className='highlight-heading'>
+								<span>{SCREEN_CONSTANTS.highlights.heading}</span>
+							</div>
+							{renderHighlight()}
+						</div>
+						<div className='about-me-options'>
+							<button
+								className='btn primary-btn'
+								onClick={() => ScrollService.scrollHandler.scrollToHireMe()}>
+								Hire Me
+							</button>
+							<a href='RajatGuptaResume2022.pdf' download='RajatGuptaResume2022.pdf'>
+								<button className='btn highlighted-btn'>Download CV</button>
+							</a>
+						</div>
+					</div>
+					<Tech idName='about-me' />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default AboutMe;
--- myportfolio/src/Container/Title_Message/TitleMessage.tsx
+++ myportfolio/src/Container/Title_Message/TitleMessage.tsx
@@ -1,89 +1,70 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
-import "./TitleMessage.css";
-import ScrollService from "../../utilities/ScrollService.ts";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
+import './TitleMessage.css';
+import ScrollService from '../../utilities/ScrollService.ts';
 
 const TitleMessage: React.FC = () => (
-  <div className="title-message">
-    <div
-      className="profile-details"
-      style={{ display: "flex", alignItems: "center", flexDirection: "column" }}
-    >
-      <div className="colz">
-        <div className="colz-icon">
-          <a href="https://www.linkedin.com/in/rajat6235/">
-            <i title="Linkedin" className="fa fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/rajat6235">
-            <i title="Github" className="fa fa-github"></i>
-          </a>
-          <a href="tel:7837733549">
-            <i title="Phone" className="fa fa-phone"></i>
-          </a>
-        </div>
-      </div>
-      <div className="profile-details-name">
-        <span className="primary-text">
-          {" "}
-          Hello, I'm <span className="highlighted-text">Rajat Gupta</span>
-        </span>
-      </div>
-      <div className="profile-details-role">
-        <span className="primary-text">
-          <h1>
-            <Typewriter
-              options={{
-                strings: [
-                  "Enthusiastic Developer 🛑",
-                  "Tech Enthusiast",
-                  "React Developer",
-                  "Front End Developer ➬ ",
-                ],
-                autoStart: true,
-                loop: true,
-                delay: 50,
-              }}
-            />
-          </h1>
-          <div
-            className="profile-role-tagline"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.16)", padding: "10px" }}
-          >
-            Experienced in crafting robust applications with frontend
-            excellence, advancing backend capabilities through practical
-            application to create comprehensive solutions.
-          </div>
-        </span>
-      </div>
-      <div className="profile-options" style={{ columnGap: '70px' }}>
-        <button
-          className="btn primary-btn"
-          onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
-          style={{
-            backgroundColor: "#1f2235",
-            border: "2px solid linen",
-            color: "white",
-            borderRadius: "50px",
-          }}
-        >
-          Hire Me
-        </button>
-        <a href="RajatGuptaResume2022.pdf" download="RajatGuptaResume2022.pdf">
-          <button
-            className="btn highlighted-btn"
-            style={{
-              backgroundColor: "#ff5823",
-              fontWeight: "400",
-              color: "white",
-              borderRadius: "50px",
-            }}
-          >
-            Download CV
-          </button>
-        </a>
-      </div>
-    </div>
-  </div>
+	<div className='title-message'>
+		<div
+			className='profile-details'
+			style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+			<div className='colz'>
+				<div className='colz-icon'>
+					<a href='https://www.linkedin.com/in/rajat6235/'>
+						<i title='Linkedin' className='fa fa-linkedin'></i>
+					</a>
+					<a href='https://github.com/rajat6235'>
+						<i title='Github' className='fa fa-github'></i>
+					</a>
+					<a href='tel:7837733549'>
+						<i title='Phone' className='fa fa-phone'></i>
+					</a>
+				</div>
+			</div>
+			<div className='profile-details-name'>
+				<span className='primary-text'>
+					Hello, I'm <span className='highlighted-text'>Rajat Gupta</span>
+				</span>
+			</div>
+			<div className='profile-details-role'>
+				<span className='primary-text'>
+					<h1>
+						<Typewriter
+							options={{
+								strings: ['UI/UX Specialist', 'Front End Pro ➬ ', 'React Developer', 'Tech Savvy'],
+								autoStart: true,
+								loop: true,
+								delay: 50,
+							}}
+						/>
+					</h1>
+					<div className='profile-role-tagline'>
+						Experienced in creating elegant and efficient frontend solutions with enhanced
+						functionality for better user interaction.
+					</div>
+				</span>
+			</div>
+			<div className='profile-options'>
+				<div className='hire-btn' onClick={() => ScrollService.scrollHandler.scrollToHireMe()}>
+					<svg>
+						<rect x='5' y='5' rx='25' fill='#1f2235' stroke='#FFF' width='150' height='50'></rect>
+            <text className='default-text' x='50%' y='50%' dominantBaseline='middle' textAnchor='middle'>
+							Hire Me
+						</text>
+						<text className='hover-text' x='50%' y='50%' dominantBaseline='middle' textAnchor='middle'>
+							Let's Connect
+						</text>
+					</svg>
+				</div>
+				<a
+					href='RajatGuptaResume2022.pdf'
+					download='RajatGuptaResume2022.pdf'
+					className='buttons-container'>
+					<button className=' button-arounder'>Download CV</button>
+				</a>
+			</div>
+		</div>
+	</div>
 );
 
 export default TitleMessage;
